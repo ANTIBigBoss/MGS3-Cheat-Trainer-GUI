d21c19f1364a08ca09cd1ded22e86ff42a8a1c41
Andrew Naro <a.naro1995@gmail.com>
2023-11-16 23:16:25 -0500

    starting to commonize and organize.

---
+++ MGS3 MC Cheat Trainer/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGS3_MC_Cheat_Trainer
+{
+    public class Constants
+    {
+        public const string PROCESS_NAME = "METAL GEAR SOLID3";
+    }
+}
